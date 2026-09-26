a66ee3f58328855ce210f7d061c7697d09b0ae0d
gcake119 <96661305+gcake119@users.noreply.github.com>
2026-09-27 01:28:30 +0800

    feat(about): 採用確認文案與社群連結

--- src/data/site.ts
+++ src/data/site.ts
@@ -12,3 +12,10 @@ export const site = {
     },
   },
 } as const;
+
+export const socialLinks = [
+  { name: 'Podcast《喂喂你還好不好》', href: 'https://open.firstory.fm/user/wwhowbuhow/platforms', icon: 'microphone' },
+  { name: 'Threads', href: 'https://www.threads.com/@wwhowbuhow', icon: 'threads' },
+  { name: 'Instagram', href: 'https://www.instagram.com/wwhowbuhow/', icon: 'instagram' },
+  { name: 'Substack', href: 'https://substack.com/@gcake119', icon: 'substack' },
+] as const;
